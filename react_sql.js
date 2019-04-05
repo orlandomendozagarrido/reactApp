96ed6e042abd14fcc0e72f55ec9d1357a85d486b
orlandomendozagarrido <orlandomendozagarrido206@gmail.com>
2019-04-04 21:34:51 -0600

    working database

--- react_sql.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  groceryList: [
-                {id: 1, quantity: 5, description: "food"},
-                {id: 2, quantity: 10, description: "tacos"},
-                {id: 3, quantity: 2, description: "soup"},
-                {id: 4, quantity: 1, description: "veggies"}
-              ]
-};
